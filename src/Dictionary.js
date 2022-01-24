3fd3aaf1cb012a40442288844dfa7d5a2fca2ece
Berlie <berl.yiadom@gmail.com>
2022-01-24 00:26:38 +0000

    Included the photo component and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,38 +1,64 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import './Dictionary.css';
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState (false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response){
-        console.log(response.data[0]);
-        setResults(response.data[0]);
+        setResults(response.data[0]); 
+    }
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);
     }
 
-    function search(event){
-        event.preventDefault();
-        
-
+    function search(){
+        // documentation: https://dictionaryapi.dev/e
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         console.log(apiUrl);
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey= "563492ad6f91700001000001cf19858656e34db9b600d7038135622d";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl,{ headers: headers}).then(handlePexelsResponse);
+
+    }
+    function handleSubmit(event){
+        event.preventDefault();
+        search();
     }
 
     function handleKeywordChange(event){
         setKeyword(event.target.value);
     }
+
+    function load() {
+        setLoaded(true);
+        search();
+    }
+    
+    if (loaded){
     return (
         <div className="Dictionary">
             <section>
-              <form className="Input" onSubmit={search}>
+              <form className="Input" onSubmit={handleSubmit}>
                 <input type="search" placeholder="Search Word" autoFocus={true} onChange={handleKeywordChange}/>
             </form>
             <span className="hint">Suggested words: hello,love,art</span>  
             </section>
+            <Photos photos= {photos}/>
             <Results results={results} />
         </div>
     );
+    }else { 
+        load();
+        return "Loading";
+    }
+    
 }
